2037d565ce60942d8a2facb39c20705f9e7b59e1
nataliealway <nlmaxwell@gmail.com>
2019-12-08 14:09:02 -0800

    created error gandlers

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 'use strict'
 
 //Application dependencies
+require('dotenv').config();
+const cors = require('cors');
+const superagent = require('superagent');
+const express = require('express');
+const app = express();
+const pg = require('pg')
+
+app.use(cors());
 
 //Configure Database
+const PORT = process.env.PORT || 3000;
+const client = new pg.Client(process.env.DATABASE_URL);
 
+//Homepage route
+function getHome(request, response) {
+  response.status(200).send('Welcome to the home page!')
+}
 //Error handler
-
-//API modules
+function notFoundHandler(request,response) {
+  response.status(404).send('huh?');
+};
+function errorHandler(error,request,response) {
+  response.status(500).send(error);
+};
 
 //API Routes
 
 //Constructors
 
-//Middleware
 
 //Server listening
+app.listen(PORT, () => console.log('Server is up and listening on PORT ${PORT}'));
